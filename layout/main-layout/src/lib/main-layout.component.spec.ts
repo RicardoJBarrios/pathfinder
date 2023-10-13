a8812a15945414ddd188b0f857d2de8a10215783
RicardoJBarrios <ricardojbarrios@gmail.com>
2023-10-13 21:21:03 +0100

    feat(app): update main layout

--- layout/main-layout/src/lib/main-layout.component.spec.ts
+++ layout/main-layout/src/lib/main-layout.component.spec.ts
@@ -1,18 +1,24 @@
 import { ActivatedRoute } from '@angular/router';
 import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { LayoutService } from '@pathfinder/shared';
 import { MockProvider } from 'ng-mocks';
 import { of } from 'rxjs';
 
 import { MainLayoutComponent } from './main-layout.component';
 import { MainLayoutService } from './main-layout.service';
 
-const mockMainLayoutService = { isHandset$: of(true), menuItems$: of([]) };
+const mockMainLayoutService = { menuItems$: of([]) };
+const mockLayoutService = { isHandset$: of(true) };
 
 describe('MainLayoutComponent', () => {
   let spectator: Spectator<MainLayoutComponent>;
   const createComponent = createComponentFactory({
     component: MainLayoutComponent,
-    providers: [MockProvider(ActivatedRoute), MockProvider(MainLayoutService, mockMainLayoutService)]
+    providers: [
+      MockProvider(ActivatedRoute),
+      MockProvider(MainLayoutService, mockMainLayoutService),
+      MockProvider(LayoutService, mockLayoutService)
+    ]
   });
 
   beforeEach(() => {
--- layout/main-layout/src/lib/main-layout.component.ts
+++ layout/main-layout/src/lib/main-layout.component.ts
@@ -1,5 +1,13 @@
 import { NgFor, NgIf } from '@angular/common';
-import { ChangeDetectionStrategy, Component, computed, DestroyRef, inject, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  DestroyRef,
+  inject,
+  ViewChild
+} from '@angular/core';
 import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,6 +15,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { LayoutService } from '@pathfinder/shared';
 import { filter } from 'rxjs';
 
 import { MainLayoutService } from './main-layout.service';
@@ -28,22 +37,24 @@ import { MenuItem } from './menu-item.type';
   styleUrls: ['./main-layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MainLayoutComponent implements OnInit {
+export class MainLayoutComponent implements AfterViewInit {
   readonly #service = inject(MainLayoutService);
+  readonly #layout = inject(LayoutService);
   readonly #destroyRef = inject(DestroyRef);
 
-  @ViewChild('sidenav', { static: true }) private readonly sidenav!: MatSidenav;
+  @ViewChild('sidenav', { static: true }) private readonly sidenav?: MatSidenav;
 
-  readonly isHandset = toSignal(this.#service.isHandset$, { initialValue: true });
+  readonly isHandset = toSignal(this.#layout.isHandset$, { initialValue: true });
   readonly menuItems = toSignal(this.#service.menuItems$, { initialValue: [] });
   readonly sidenavMode = computed(() => (this.isHandset() ? 'over' : 'side'));
+  readonly topGap = computed(() => (this.isHandset() ? 48 : 0));
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.#closeSidenavIfNoHandset();
   }
 
   toggleSidenav(): void {
-    this.sidenav.toggle();
+    this.sidenav?.toggle();
   }
 
   trackMenuByLabel(index: number, item: MenuItem): string {
@@ -51,13 +62,13 @@ export class MainLayoutComponent implements OnInit {
   }
 
   #closeSidenavIfNoHandset(): void {
-    this.#service.isHandset$
+    this.#layout.isHandset$
       .pipe(
-        filter((isHandset: boolean) => isHandset === false),
+        filter((isHandset: boolean) => isHandset === false && Boolean(this.sidenav?.opened)),
         takeUntilDestroyed(this.#destroyRef)
       )
       .subscribe(() => {
-        this.sidenav.close();
+        this.sidenav?.close();
       });
   }
 }
